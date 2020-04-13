f470dbb2e0c60920f64a340a0738fa91f7f9e77e
Nguyen Tan Dat <dat.nguyen_bk@hcmut.edu.vn>
2020-04-13 11:09:09 +0700

    ADDED: sendThread

---
+++ src/client/SendThread.java
@@ -0,0 +1,17 @@
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class SendThread extends Thread {
+	private OutputStream outputStream;
+	
+	public SendThread(OutputStream out) {
+		this.outputStream = out;
+	}
+	
+	public void send(String msg) throws IOException {
+		outputStream.write(msg.getBytes());
+	}
+}
